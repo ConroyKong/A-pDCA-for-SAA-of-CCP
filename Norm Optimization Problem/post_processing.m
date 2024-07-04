3ae8220952c03d3cc03b4089357da7cac409b0d1
Qingyuan Kong <36792115+ConroyKong@users.noreply.github.com>
2024-07-04 15:57:59 +0800

    Delete Norm Optimization Problem directory

--- Norm Optimization Problem/post_processing.m
+++
@@ -1,6 +0,0 @@
-function [val_mean, val_min, val_max] = post_processing(val_collector)
-val_collector = val_collector(find(val_collector));
-val_mean = mean(val_collector);
-val_min = min(val_collector);
-val_max = max(val_collector);
-end
